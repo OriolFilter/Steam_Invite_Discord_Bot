22dcc870bca1fd46e36cf52c1f0c599d0ed60955
savagebidoof <filter.oriol@gmail.com>
2023-12-20 03:53:33 +0100

    Added `GOD_DISCORD_ID` environment.

--- code/Classes.py
+++ code/Classes.py
@@ -66,12 +66,15 @@ class DiscordConf(_CONFIG):
     prefix: str = "s."
     description: str = ""
     activity: str = ""
+    god_id: int = None
 
     def load_envs(self):
         self.token = getenv("DISCORD_TOKEN", self.token)
         self.prefix = getenv("DISCORD_PREFIX", self.prefix)
         self.description = getenv("DISCORD_DESCRIPTION", self.description)
         self.activity = getenv("DISCORD_ACTIVITY", self.activity)
+        self.activity = getenv("DISCORD_ACTIVITY", self.activity)
+        self.god_id = getenv("GOD_DISCORD_ID", self.god_id)
         return self
 
 
