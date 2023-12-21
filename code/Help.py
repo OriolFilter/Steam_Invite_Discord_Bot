dad3af59ffe289e05c1b0f08f976b74637eb8e6a
savagebidoof <filter.oriol@gmail.com>
2023-12-21 06:28:37 +0100

    Modified some images used on the help command.

--- code/Help.py
+++ code/Help.py
@@ -6,78 +6,6 @@ from discord import Embed
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-# txt_help_help = """
-# ```md
-# ### Bot commands commands
-# lobby           Returns the lobby of the user. A discord user can be mentioned to return their lobby. If the short link (shlink) functionality is enabled on the bot, the lobby link will be linked to a link shortener service and allow the users to click on the Steam link.
-# profile         Returns the profile of the user and their active game. A discord user can be mentioned to return their profile.
-# shlink          Behaves like the `lobby` command, but instead of returning "steam match link", returns the link shortener URL
-#
-# ### Account bindings
-# link            Sets up your account providing the vanity url. If you need help setting it up use the command `help link`.
-# unlink          Use this to unlink the account and will delete the database entry.
-#
-# ### Admin commands
-# sync            Sync the commands with all the servers (Bot Owner only).
-#
-# ### Help commands
-# help            Prints a list of commands and their description
-# help link       How to link your Steam account with this bot.
-# help lobby      How to use the lobby command (and shlink)
-# help usage      Example on how to use this bot/Commands and stuff (sort of functionalities)
-#
-# ### Miscellany
-# invite_bot       Returns a link to invite this bot to your server.
-# botinvite       Returns a link to invite this bot to your server.
-# version         Prints the current version of the bot
-#
-# ### Available Slash/App commands
-# - help
-# - link
-# - lobby
-# - profile
-# - shlink
-# - unlink
-# ```
-# > Note: To use Slash/Apps commands type `/` on the chat and proceed to select the desired command.
-# """
-#
-# # **__Title:__**
-# # help            Returns this list. This also has an alias `help general`
-#
-# zombie = """
-# # Title
-# """
-#
-# _text_test = {
-#     "Bot commands commands":
-#         """
-#     ```
-#     lobby           Returns the lobby of the user. A discord user can be mentioned to return their lobby. If the short link (shlink) functionality is enabled on the bot, the lobby link will be linked to a link shortener service and allow the users to click on the Steam link.
-#     profile         Returns the profile of the user and their active game. A discord user can be mentioned to return their profile.
-#     shlink          Behaves like the `lobby` command, but instead of returning "steam match link", returns the link shortener URL
-#     ```
-#     """,
-#
-#     "Account bindings":
-#         """
-#     ```
-#     link            Sets up your account providing the vanity url. If you need help setting it up use the command `help link`.
-#     unlink          Use this to unlink the account and will delete the database entry.
-#     ```
-#     """
-# }
-#
-# _text_test2 = {
-#     "Bot commands commands":
-#         {
-#             "lobby": "Returns the lobby of the user. A discord user can be mentioned to return their lobby. If the short link (shlink) functionality is enabled on the bot, the lobby link will be linked to a link shortener service and allow the users to click on the Steam link.",
-#             "profile": "Returns the profile of the user and their active game. A discord user can be mentioned to return their profile.",
-#             "shlink": 'Behaves like the `lobby` command, but instead of returning "steam match link", returns the link shortener URL',
-#         }
-#
-# }
-
 _help_commands = {
     ":tanabata_tree: __**Main Commands**__":
         [
@@ -111,81 +39,6 @@ _additional_commands = {
 
 _available_app_commands = ["help", "link", "lobby", "profile", "shlink", "unlink"]
 _available_app_commands.sort()
-
-txt_help_link = """
-Using as reference my own Steam Account.
-
-To obtain your vanity URL is as simple as going to your profile page, right click on the background and click "copy URL".
-
-It should return something as the following:
-
-`https://steamcommunity.com/id/SavageBidoof/`
-
-Now we just need grab from that URL, the account name displayed.
-
-Like: https://i.imgur.com/VHdVEj8.png
-
-Once we have copied such part, we can proceed to linking the account with the `link` command.
-
-Based on my Steam link, I would be using:
-
-`s.link savagebidoof`
-
-Note: This command is not sensitive to upper and lower case so write whatever you want.
-
-Also, the prefix from the command I used might be different from the one running on this bot.
-
-Couple images as reference (note that outputs and command prefix might vary):
-
-https://i.imgur.com/jiYtPVN.png
-
-https://i.imgur.com/7Ff5rzz.png
-
-Regarding unlinking the account, the output will "always" be the same (as long it worked correctly).
-
-As much there could be issues connecting to the database, in such scenario an error will be risen accordingly.
-
-https://i.imgur.com/J7UUUPD.png
-"""
-
-txt_help_usage = """
-```md
-1. Link your Steam account to the Discord bot (doesn't require login nor authentication or anything like that), for more information about this step use the command `help link`.
-2. Congrats you can now use the rest of commands, such as:
-- Lobby
-- Profile
-- Shlink (if functionality enabled)
-```
-
-An image for reference, note that outputs and command prefix might vary (as well I would recommend to open the image on a new browser tab as otherwise might be blurry):
-https://i.imgur.com/liZl6fI.png
-"""
-
-txt_help_lobby = """
-The `lobby` command returns the Steam account of the user, their current active game, and their lobby (if there is one).
-
-You can also target another user to display their lobby.
-
-If the `shlink` (short links) functionality is enabled, links will be clickable and redirect towards the Steam lobby.
-
-
-The `shlink` command does the same functionality as `lobby`, but the text displayed will be the short link, this is most useful if you wanna copy paste the short link to the lobby.
-
-On both instances you can target another user to display their profile.
-
-https://i.imgur.com/0D83hsz.png
-
-https://i.imgur.com/BsjU1ps.png
-"""
-
-txt_help_profile = """
-The `profile` command returns the Steam account of the user, and their current active game (if there is one).
-
-You can also target another user to display their profile.
-
-https://i.imgur.com/aVPfFzR.png
-"""
-
 
 # HELP_DIC = {
 #     'general': txt_help_help,
@@ -290,7 +143,8 @@ class HELPER:
                f"their profile.")
 
         embed.add_field(name="", value=txt)
-        images = ["https://i.imgur.com/aVPfFzR.png"]
+        # images = ["https://i.imgur.com/aVPfFzR.png"]
+        images = ["https://i.imgur.com/bX30fbA.png"]
         for _image_url in images:
             _embed = self.__return_embed_image_template
             _embed.set_image(url=_image_url)
@@ -345,7 +199,8 @@ class HELPER:
         txt = f"Use commands such **{self.__discord_bot.command_prefix}lobby** or **{self.__discord_bot.command_prefix}shlink** to post a lobby.\n\nYou can select another user to post their lobby."
 
         embed.add_field(name="", value=txt, inline=False)
-        images = ["https://i.imgur.com/BsjU1ps.png", "https://i.imgur.com/0D83hsz.png"]
+        images = ["https://i.imgur.com/VWk90iV.png", "https://i.imgur.com/w5DN6m0.png"]
+        # images = ["https://i.imgur.com/BsjU1ps.png", "https://i.imgur.com/0D83hsz.png"]
         for _image_url in images:
             _embed = self.__return_embed_image_template
             _embed.set_image(url=_image_url)
