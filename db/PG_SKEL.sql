16d12b8f49c7beb08328796f539fbf00b77cd519
savagebidoof <filter.oriol@gmail.com>
2023-12-27 04:56:31 +0100

    Set back trigger `trigger_set_last_update_now` to keep track of modifications.

--- db/PG_SKEL.sql
+++ db/PG_SKEL.sql
@@ -6,35 +6,17 @@ create table if not exists relationships(
     last_update timestamp null -- last time it was updated
 );
 
--- create or replace function trigger_check_values_relationships()
--- returns Trigger
--- language plpgsql
--- as $$
---     begin
---     case when length(NEW.discord_id)!=18 then
---         RAISE 'discord_id not correct length: % instead of 18', length(new.discord_id) USING ERRCODE = '22026';
---         when length(NEW.steam_id)!=17 then
---         RAISE 'steam_id not correct length: % instead of 18', length(new.steam_id) USING ERRCODE = '22026';
---         else
---             new.last_update=NOW();
---             return new;
---     end case;
---     end;
--- $$;
+create or replace function trigger_set_last_update_now()
+returns Trigger
+language plpgsql
+as $$
+    begin
+        update relationships as rel set last_update=now() where new.id=rel.id;
+    end;
+$$;
 
--- create or replace function trigger_set_last_update_now()
--- returns Trigger
--- language plpgsql
--- as $$
---     begin
---         update relationships as rel set last_update=now() where new.id=rel.id;
---     end;
--- $$;
-
--- create trigger check_values_insert before insert on relationships for each row execute procedure trigger_check_values_relationships();
--- create trigger check_values_update before update on relationships for each row execute procedure trigger_check_values_relationships();
--- create trigger check_values_update after insert on relationships for each row execute procedure trigger_set_last_update_now();
--- create trigger check_values_update after update on relationships for each row execute procedure trigger_set_last_update_now();
+create trigger check_values_update after insert on relationships for each row execute procedure trigger_set_last_update_now();
+create trigger check_values_update after update on relationships for each row execute procedure trigger_set_last_update_now();
 
 create or replace function func_discord_id_exists(arg_discord_id varchar(18))
 returns bool
